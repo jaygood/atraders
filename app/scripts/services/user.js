b905cb1fec7bb8b10a833c70652dfc3b2db76e2e
Jon <haygoodjon@gmail.com>
2014-10-20 11:08:20 -0700

    Clean files

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -79,4 +79,9 @@ angular.module('frameworkApp')
           deferred.reject(message);
         })
     };
+
+    this.isValidEmail = function(email){
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    };
   })
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,20 +3,23 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('frameworkAppApp'));
+  beforeEach(module('frameworkApp'));
 
-  var MainCtrl,
-    scope;
+  var ctrl,
+      scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    ctrl = $controller('MainCtrl', {
       $scope: scope
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
+  });
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings[1]).toBe('two');
   });
 });
