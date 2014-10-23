47df281bdd64c88ad5d242ad523a0dfeb3902747
Jon <haygoodjon@gmail.com>
2014-10-22 20:58:31 -0700

    Implement new active tab directive

--- app/scripts/directives/activetab.js
+++ app/scripts/directives/activetab.js
@@ -15,11 +15,36 @@ angular.module('frameworkApp')
       template: '<li><a ng-href="#{{::link.ref}}" ng-transclude></a></li>',
       link: function(scope, element) {
         scope.$on('$routeChangeSuccess', function (event, current) {
-          if (current.$$route.originalPath === scope.link.ref){ //attrs['jdInfo'])
+          if (current.$$route.originalPath === scope.link.ref){
             element.addClass(scope.link.activeClass);
           }
           else{
             element.removeClass(scope.link.activeClass);
+          }
+        });
+      }
+    };
+  })
+
+  // <ul class="nav nav-pills pull-right" jd-active-class="active">
+  //   <li><a ng-href="#/">Home</a></li>
+  // </ul>
+  .directive('jdActiveClass', function () {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs){
+        scope.$on('$routeChangeSuccess', function (event, current) {
+          var elem = element[0],
+              activeClass = attrs.jdActiveClass,
+              length = elem.children.length,
+              child;
+          for(var i = 0; i < length; i++){
+            child = elem.children[i];
+            if ('#' + current.$$route.originalPath === child.firstChild.hash){
+              angular.element(child).addClass(activeClass);
+            }else{
+              angular.element(child).removeClass(activeClass);
+            }
           }
         });
       }
