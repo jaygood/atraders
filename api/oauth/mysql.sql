a2acb6e56380a82e296abee4ce07355fae544859
Jon <haygoodjon@gmail.com>
2014-10-27 18:40:04 -0700

    Authorization up and running

--- api/oauth/mysql.sql
+++ api/oauth/mysql.sql
@@ -1,7 +1,6 @@
-CREATE TABLE `creds` (
+CREATE TABLE `items` (
   `id` CHAR(40) NOT NULL,
   `name` CHAR(40) NOT NULL,
-  `password` CHAR(40) NOT NULL,
-  `key` CHAR(40) NOT NULL,
+  `phrase` CHAR(40) NOT NULL,
   PRIMARY KEY (`id`)
 );
