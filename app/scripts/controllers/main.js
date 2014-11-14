dfe36e7498bebe3e6ab1953bb01152f3c39cb1fe
Jon <haygoodjon@gmail.com>
2014-11-14 13:51:55 -0800

    Update header to include dropdown menu

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,11 +5,5 @@
 'use strict';
 angular.module('frameworkApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.one = 1;
-    $scope.two = 2;
-    $scope.awesomeThings = [
-      'one',
-      'two',
-      'three'
-    ];
+
   }]);
